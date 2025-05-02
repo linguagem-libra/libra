8dc25ec4f41952062c4e8f68b230b8448eeed6b6
Lucas M. Campos <lucasm.campos@hotmail.com.br>
2025-05-02 17:03:43 -0300

    Atualiza rodar.sh

--- rodar.sh
+++ rodar.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 
+# Criar diretório build se não existir
 mkdir -p build
 
-gcc libra/*.c -o build/librac && ./build/librac
+# Compilar arquivos em cli/ e libra/ com inclusão do diretório libra/
+gcc cli/*.c libra/*.c -Ilibra -o build/librac && ./build/librac
