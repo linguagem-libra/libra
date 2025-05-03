f811385e3130f1d1adf016181bb512333bdb2c8c
Lucas M. Campos <lucasm.campos@hotmail.com.br>
2025-05-02 21:14:40 -0300

    Adiciona libra_copiar_mem()

--- libra/memoria.c
+++ libra/memoria.c
@@ -1,3 +1,5 @@
+#include <string.h>
+#include <stdio.h>
 #include <malloc.h>
 #include "memoria.h"
 #include "erro.h"
@@ -15,6 +17,11 @@ void* libra_realocar(void* ptr, size_t size)
   void *novo_ptr = realloc(ptr, size);
   if (!novo_ptr) libra_erro("Sistema sem memória disponível");
   return novo_ptr;
+}
+
+void libra_copiar_mem(void* destino, void* origem, size_t num_bytes)
+{
+  memcpy(destino, origem, num_bytes);
 }
 
 void libra_liberar(void *ptr)
